0df1558520db7eb581c0f6aa5f2de5fc1569b757
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-02-09 00:51:12 -0300

    validation customer exists

--- primeiro-projeto-nodejs/src/index.js
+++ primeiro-projeto-nodejs/src/index.js
@@ -9,12 +9,19 @@ const customers = [];
 
 app.post('/account', (req, res) => {
   const { name, cpf } = req.body;
-  const id = uuidv4();
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ error: 'Customer already exists!' });
+  }
 
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: [],
   });
 
