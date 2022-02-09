6f60bb637f9d12367d4e6a10994d106e821332ca
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-02-09 01:25:23 -0300

    create middleware

--- primeiro-projeto-nodejs/src/index.js
+++ primeiro-projeto-nodejs/src/index.js
@@ -7,6 +7,18 @@ app.use(express.json());
 
 const customers = [];
 
+//Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  if (!customer) {
+    return res.status(400).send({ error: 'Customer not found' });
+  }
+
+  req.customer = customer;
+  return next();
+}
+
 app.post('/account', (req, res) => {
   const { name, cpf } = req.body;
 
@@ -28,15 +40,8 @@ app.post('/account', (req, res) => {
   return res.status(201).send();
 });
 
-app.get('/statement/', (req, res) => {
-  const { cpf } = req.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return res.status(400).send({ error: 'Customer not found' });
-  }
-
+app.get('/statement/', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
   return res.json(customer.statement);
 });
 
